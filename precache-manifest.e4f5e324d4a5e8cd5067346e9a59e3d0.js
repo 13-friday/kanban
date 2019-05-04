d7dd73ccc7cf929b6b915dba1bb4f1ab755e61ce
Pytniza <ip.web.so@gmail.com>
2019-05-04 14:14:08 +0300

    Updates

---
+++ precache-manifest.e4f5e324d4a5e8cd5067346e9a59e3d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9045dea9973ce2287825fdf708a698e2",
+    "url": "/kanban/index.html"
+  },
+  {
+    "revision": "a3e2bfcd4c6c090fddde",
+    "url": "/kanban/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/kanban/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "a3e2bfcd4c6c090fddde",
+    "url": "/kanban/static/js/main.8784d7fc.chunk.js"
+  },
+  {
+    "revision": "9c81d15565ccb12a85c0",
+    "url": "/kanban/static/js/runtime~main.4f25e9d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kanban/static/media/logo.5d5d9eef.svg"
+  }
+]);
